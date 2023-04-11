103748e7fb6ae84295b3f10c3f9721e3860e6f35
Coppelia <mfreese@gmx.ch>
2023-04-11 16:51:08 +0200

    small refact + fvisibility=hidden

--- simExtCHAI3D.h
+++ simExtCHAI3D.h
@@ -67,12 +67,7 @@
 */
 //---------------------------------------------------------------------------
 
-#ifdef _WIN32
-    #define SIM_DLLEXPORT extern "C" __declspec(dllexport)
-#endif
-#if defined (__linux) || defined (__APPLE__)
-    #define SIM_DLLEXPORT extern "C"
-#endif
+#include <simLib/simExp.h>
 
 SIM_DLLEXPORT unsigned char simStart   (void* reservedPointer, int reservedInt);
 SIM_DLLEXPORT void          simEnd     ();
